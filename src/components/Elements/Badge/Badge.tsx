67090529ac268cbb023e81fcdbeb2aeeee1d5c1b
Gregor Weichbrodt <gambolputty@users.noreply.github.com>
2022-03-07 18:02:55 +0100

    make badges color darker

--- src/components/Elements/Badge/Badge.tsx
+++ src/components/Elements/Badge/Badge.tsx
@@ -2,8 +2,8 @@ import clsx from "clsx";
 
 const variants = {
   indigo: "bg-indigo-100 text-indigo-800",
-  orange: "bg-orange-100 text-orange-800",
-  gray: "bg-gray-100 text-gray-800",
+  orange: "bg-orange-200 text-orange-800",
+  gray: "bg-gray-200 text-gray-800",
   red: "bg-red-100 text-red-800",
   yellow: "bg-yellow-100 text-yellow-800",
   green: "bg-green-100 text-green-800",
